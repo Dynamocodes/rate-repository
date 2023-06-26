cbf82f690a6493b6db56dc9a2979df7d5a0904a0
dynamo <hietanenelias@gmail.com>
2023-06-26 12:04:20 +0200

    exercise 10.5

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,15 +1,23 @@
 import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import theme from "../theme";
 
 const Main = () => {
+  const styles = StyleSheet.create({
+    container:{
+      backgroundColor: theme.backgroundColors.backgroundSecondary,
+      paddingBottom: 5,
+      flexGrow: 1,
+    },
+  });
+
   return (
-    <View>
+    <View style={styles.container}>
       <AppBar/>
       <RepositoryList/>
     </View>
-
+    
   );
 };
-
 export default Main;
--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,18 +1,98 @@
-import { View} from 'react-native'
+import { View, Image, StyleSheet} from 'react-native'
 import Text from './Text'
+import theme from '../theme';
 
 const RepositoryItem = ({item}) => {
+
+  const styles = StyleSheet.create({
+    topContainer:{
+      flexDirection: 'row',
+    },
+    topLeftContainer:{
+      padding: 5,
+    },
+    topRightContainer:{
+      padding: 5,
+    },
+    bottomContainer:{
+      display: 'flex',
+      flexDirection:'row',
+      justifyContent:'space-around',
+      padding: 5,
+    },
+    bottomSubContainer:{
+      display:'flex',
+      flexDirection: 'column',
+      alignSelf:'flex-start',
+      alignItems: 'center',
+    },
+    tag:{
+      backgroundColor: theme.colors.primary,
+      padding:10,
+      flexGrow:0,
+      borderRadius:5,
+      alignSelf:'flex-start',
+    },
+    mainContainer: {
+      padding: 5,
+      marginBottom:6,
+      backgroundColor: theme.backgroundColors.backgroundPrimary,
+    },
+    tinyLogo: {
+      width: 50,
+      height: 50, 
+      borderRadius:7,
+    },
+    logo: {
+      width: 66,
+      height: 58,
+    },
+  });
+
+  const formatAmount = (num) => {
+    if(num >= 1000){
+      return `${Math.round((num/1000) * 10)/10}k`
+    }
+    return `stars`
+  }
+
   return (
-    <View>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Descriptiopn: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.topContainer}>
+        <View style={styles.topLeftContainer}>
+          <Image
+          source={{
+            uri:`${item.ownerAvatarUrl}`
+          }}
+          style={styles.tinyLogo}/>
+        </View>
+        <View style={styles.topRightContainer}>
+          <Text fontWeight="bold">{item.fullName}</Text>
+          <Text color="textSecondary">{item.description}</Text>
+          <View style={styles.tag}>
+            <Text color="highContrast" >{item.language}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.bottomSubContainer}>
+          <Text fontWeight='bold'>{formatAmount(item.stargazersCount)}</Text>
+          <Text color="textSecondary">Stars</Text>
+        </View>
+        <View style={styles.bottomSubContainer}>
+          <Text fontWeight='bold'>{formatAmount(item.forksCount)}</Text>
+          <Text color="textSecondary">Forks</Text>
+        </View>
+        <View style={styles.bottomSubContainer}>
+          <Text fontWeight='bold'>{item.reviewCount}</Text>
+          <Text color="textSecondary">Reviews</Text>
+        </View>
+        <View style={styles.bottomSubContainer}>
+        <Text fontWeight='bold'>{item.ratingAverage}</Text>
+          <Text color="textSecondary">Rating</Text>
+        </View>
+      </View>
     </View>
   )
 }
-
 export default RepositoryItem
