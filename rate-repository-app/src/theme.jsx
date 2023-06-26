eb7f1e1cd17f4160f50122537991a2f8d9514d4d
dynamo <hietanenelias@gmail.com>
2023-06-26 12:42:29 +0200

    exercise 10.10

--- rate-repository-app/src/theme.jsx
+++ rate-repository-app/src/theme.jsx
@@ -1,27 +1,33 @@
-const theme = {
-  colors: {
-    textPrimary: '#24292e',
-    textSecondary: '#586069',
-    primary: '#0366d6',
-    highContrast: '#ffffff',
-    textError: '#d73a4a'
-  },
-  backgroundColors:{
-    dark:"#24292e",
-    backgroundSecondary: '#e1e4e8',
-    backgroundPrimary: '#ffffff'  
-  },
-  fontSizes: {
-    body: 14,
-    subheading: 16,
-  },
-  fonts: {
-    main: 'System',
-  },
-  fontWeights: {
-    normal: '400',
-    bold: '700',
-  },
-};
+import { Platform } from "react-native";
 
-export default theme;
+const theme = {
+    colors: {
+      textPrimary: '#24292e',
+      textSecondary: '#586069',
+      primary: '#0366d6',
+      highContrast: '#ffffff',
+      textError: '#d73a4a'
+    },
+    backgroundColors:{
+      dark:"#24292e",
+      backgroundSecondary: '#e1e4e8',
+      backgroundPrimary: '#ffffff'  
+    },
+    fontSizes: {
+      body: 14,
+      subheading: 16,
+    },
+    fonts: {
+      main: Platform.select({
+        android: 'Roboto',
+        ios: 'Arial',
+        default: 'System',
+      }),
+    },
+    fontWeights: {
+      normal: '400',
+      bold: '700',
+    },
+  };
+  
+  export default theme;
