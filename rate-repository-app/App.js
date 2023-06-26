af941bbb00085ddd0fcd8aea5a902199278fe63e
dynamo <hietanenelias@gmail.com>
2023-06-26 16:29:23 +0200

    exercise 10.12

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,16 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
   return (
-    <>
-      <NativeRouter>
+    <NativeRouter>
+      <ApolloProvider client={apolloClient}>
         <Main />
-      </NativeRouter>
-      <StatusBar style="auto" />
-    </>
+      </ApolloProvider>
+    </NativeRouter>
   );
 };
 
---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const allRepositories = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+    // Other options
+  })
+
+  const repositories = allRepositories.loading ? undefined : allRepositories.data.repositories
+  
+  const loading = allRepositories.loading;
+
+
+  return { repositories, loading };
+};
+
+export default useRepositories;
