7b3075c3239c2285528b128fb8934e5610a34c05
dynamo <hietanenelias@gmail.com>
2023-06-26 12:15:55 +0200

    exercise 10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,9 +1,8 @@
-import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import Text from './Text';
 import theme from '../theme';
-
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
@@ -19,28 +18,25 @@ const styles = StyleSheet.create({
 
 const AppBarTab = ({title, path}) => {
   return(
-    <ScrollView horizontal>
-      <Pressable onPress={()=>console.log(`${title} pressed.`)}>
-        <Link to={path}>
-          <Text 
-          fontSize="subheading" 
-          fontWeight="bold" 
-          color="highContrast"
-          style={styles.tab}>
-            {title}
-          </Text>
-        </Link>
-      </Pressable>
-    </ScrollView>
-
+    <Link to={path}>
+      <Text 
+      fontSize="subheading" 
+      fontWeight="bold" 
+      color="highContrast"
+      style={styles.tab}>
+        {title}
+      </Text>
+    </Link>
   )
 }
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab title="Repository" path="/"/>
-      <AppBarTab title="Sign In" path="/sign-in"/>
+      <ScrollView horizontal>
+        <AppBarTab title="Repository" path="/"/>
+        <AppBarTab title="Sign In" path="/sign-in"/>
+      </ScrollView>
     </View>
   );
 };
